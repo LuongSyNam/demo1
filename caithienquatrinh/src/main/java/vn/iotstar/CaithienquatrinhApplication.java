f83751556baec78a6f436068030e1c6fd624d0c7
PC <PC@DESKTOP-1MKJ06O>
2024-11-20 14:29:05 +0700

    First Commit

---
+++ caithienquatrinh/src/main/java/vn/iotstar/CaithienquatrinhApplication.java
@@ -0,0 +1,13 @@
+package vn.iotstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaithienquatrinhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaithienquatrinhApplication.class, args);
+	}
+
+}
